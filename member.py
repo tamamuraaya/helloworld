518150d92e38aab8c59e68c50abdf67f063ec951
321D24028 <139970100+321D24028@users.noreply.github.com>
2023-07-20 11:31:26 +0900

    変更しました
    房州優樹を追加

--- member.py
+++ member.py
@@ -1,4 +1,4 @@
-ver = "0.1"
+ver = "0.2"
 
 class Member:
     def __init__(self, name, words=""):
@@ -17,8 +17,8 @@ newmember = Member("江頭2:50", "エガちゃんです！")
 mlist.append(newmember)
 
 ### 以下に自分を追加する ###
-
-
+newmember = Member("房州優樹", "よろしくです！")
+mlist.append(newmember)
 
 # メンバー表示
 print("各メンバーから一言")
