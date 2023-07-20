487d3c2a18e83aa8ce064d5c48209e05da6e53ca
yosidagumi <321D24034@edu.thu.ac.jp>
2023-07-20 11:37:42 +0900

    add new comment
    吉田を追加

--- member.py
+++ member.py
@@ -16,11 +16,11 @@ mlist = []
 newmember = Member("江頭2:50", "エガちゃんです！")
 mlist.append(newmember)
 
-<<<<<<< HEAD
+
 ### 以下に自分を追加する ###
 newmember = Member("房州優樹", "よろしくです！")
 mlist.append(newmember)
-=======
+
 
 ### 以下に自分を追加する ###
 newmember = Member("吉田 羅生", "よろしくです！")
@@ -30,7 +30,7 @@ newmember = Member("吉田 羅生", "よろしくです！")
 newmember = Member("大河原翔太", "よろしくお願いいたします。")
 mlist.append(newmember)
 
->>>>>>> 901294554bfb4975aa723a8c5df746849f91fc29
+
 
 # メンバー表示
 print("各メンバーから一言")
